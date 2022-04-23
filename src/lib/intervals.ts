1a5cd42d5d9e0f462a871ebe37dcdab2d3fe7c2e
unknown <timsch3@gmail.com>
2022-04-23 12:02:47 +0200

    set hunger interval back to normal

--- src/lib/intervals.ts
+++ src/lib/intervals.ts
@@ -1,6 +1,6 @@
 export const intervals = {
   // change for testing
-  hunger: 5, // 5 hours: 18000
+  hunger: 18000, // 5 hours: 18000
   loneliness: 27000, // 7.5 hours: 27000
   dirtiness: 43200, // 12 hours: 43200
   health: 36000, // 10 hours: 36000
